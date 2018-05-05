dc4ee355168dbe43337aa8d51b6b241314566fd8
binliu-base <2598725694@qq.com>
2018-05-05 22:25:11 +0800

    initial commit

--- helper.py
+++ helper.py
@@ -1,23 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def show_all_keypoints(image, predicted_key_pts):
-    """Show image with predicted keypoints"""
-    # image is grayscale
-    plt.imshow(image, cmap='gray')
-    plt.scatter(predicted_key_pts[:, 0], predicted_key_pts[:, 1], s=40, marker='.', c='m')
+# def show_all_keypoints(image, predicted_key_pts):
+#     """Show image with predicted keypoints"""
+    # # image is grayscale
+    # plt.imshow(image, cmap='gray')
+    # plt.scatter(predicted_key_pts[:, 0], predicted_key_pts[:, 1], s=40, marker='.', c='m')
 
-def visualize_output(test_image, test_outputs, i):
+def visualize_output(figure, test_image, test_output, i):
 
-    plt.figure(figsize=(20,10))
-    ax = plt.subplot(1, 2, i+1)
-
+    s = test_image.shape[0]/2
     # un-transform the predicted key_pts data
-    predicted_key_pts = test_outputs.data
-    predicted_key_pts = predicted_key_pts.cpu().numpy()
+    predicted_key_pts = test_output.data
+    # predicted_key_pts = predicted_key_pts.cpu().numpy()
+    predicted_key_pts = predicted_key_pts.cpu().numpy()    
     # undo normalization of keypoints      
-    predicted_key_pts = predicted_key_pts*50.0+100
-
+    predicted_key_pts = (predicted_key_pts + 0.999)*s
+   
     # call show_all_keypoints
-    show_all_keypoints(np.squeeze(test_image), predicted_key_pts[0])
-    plt.axis('on')
+    # show_all_keypoints(np.squeeze(test_image), predicted_key_pts[0])
+    # plt.axis('on')  
+
+    # image is grayscale
+    plt.imshow(test_image, cmap='gray')
+    plt.scatter(predicted_key_pts[0][:, 0], predicted_key_pts[0][:, 1], s=40, marker='.', c='m')
+    
--- models.py
+++ models.py
@@ -20,32 +20,43 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")      
         self.conv1 = nn.Conv2d(1, 32, 3)
         self.conv2 = nn.Conv2d(32, 64 , 3)  
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)        
 
-        self.conv3 = nn.Conv2d(64, 64, 3)
-        self.conv4 = nn.Conv2d(64, 128, 3)        
+        self.bn1 = nn.BatchNorm2d(32)           
+        self.bn2 = nn.BatchNorm2d(64)                   
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)        
+        self.f1_bn = nn.BatchNorm1d(1000)                                                              
+        self.f2_bn = nn.BatchNorm1d(1000)                                                                      
+        # self.conv2_drop = nn.Dropout2d(p=0.5)                
+        self.conv2_drop = nn.Dropout2d(p=0.5, inplace=True)                        
+        self.pool = nn.MaxPool2d(2, 2)     
 
-        self.conv5 = nn.Conv2d(128, 128, 3)
-        self.conv6 = nn.Conv2d(128, 128, 3)
+        self.fc1 = nn.Linear(256 * 8 * 8, 1000)
+        # self.fc2 = nn.Linear(3000, 1000)          
+        self.fc3 = nn.Linear(1000, 136)  
 
-        self.conv2_drop = nn.Dropout2d(p=0.5)                
-        self.pool = nn.MaxPool2d(2, 2)        
-
-        self.fc1 = nn.Linear(128 * 17 * 17, 1024)
-        self.fc2 = nn.Linear(1024, 136)  
         
 
     def forward(self, x):
-        if torch.cuda.is_available():
-            x = x.cuda()
 
-        x = self.conv2_drop(self.pool(F.relu(self.conv2(F.relu(self.conv1(x))))))
-        x = self.conv2_drop(self.pool(F.relu(self.conv4(F.relu(self.conv3(x))))))
-        x = self.conv2_drop(self.pool(F.relu(self.conv6(F.relu(self.conv5(x))))))        
-        x = x.view(-1, 128 * 17 * 17)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = x.to(self.device)
+        # x = self.conv2_drop(self.pool(F.leaky_relu(self.bn2(self.conv2(F.leaky_relu(self.conv1(x), True))),True)))
+        # x = self.conv2_drop(self.pool(F.leaky_relu(self.bn4(self.conv4(F.leaky_relu(self.conv3(x), True))),True)))
+        # x = self.conv2_drop(self.pool(F.leaky_relu(self.bn6(self.conv6(F.leaky_relu(self.conv5(x), True))),True)))
+
+        x = self.conv2_drop(self.pool(F.leaky_relu(self.bn1(self.conv1(x)))))
+        x = self.conv2_drop(self.pool(F.leaky_relu(self.bn2(self.conv2(x)))))
+        x = self.conv2_drop(self.pool(F.leaky_relu(self.bn3(self.conv3(x)))))
+        x = self.conv2_drop(self.pool(F.leaky_relu(self.bn4(self.conv4(x)))))                        
+                
+        x = x.view(-1, 256 * 8 * 8)
+        x = F.leaky_relu(self.f1_bn(self.fc1(x)))
+        # x = F.leaky_relu(self.f2_bn(self.fc2(x)))
+        x = self.fc3(x)
             
         return x
